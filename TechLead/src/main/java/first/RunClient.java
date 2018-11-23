6044a28e65689307cf74b998f992ceaf2f763698
ChegoVdrug <41072803+ChegoVdrug@users.noreply.github.com>
2018-11-23 16:39:55 +0300

    Добавила запускатели

---
+++ TechLead/src/main/java/first/RunClient.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.io.IOException;
+
+public class RunClient {
+    public static void main (String... args) throws IOException {
+
+      Client.startClient();
+
+
+    }
+
+
+}
---
+++ TechLead/src/main/java/first/RunServer.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.io.IOException;
+
+public class RunServer {
+    public static void main (String... args) throws IOException {
+
+      Server.startServer();
+
+
+    }
+
+
+}
