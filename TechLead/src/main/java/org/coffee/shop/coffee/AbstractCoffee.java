83289d7170ee42b6d4ff3e48f40887730682b75b
ChegoVdrug <41072803+ChegoVdrug@users.noreply.github.com>
2018-12-12 14:22:26 +0300

    # рефакторинг вывода из Map

--- TechLead/src/main/java/org/coffee/shop/coffee/AbstractCoffee.java
+++ TechLead/src/main/java/org/coffee/shop/coffee/AbstractCoffee.java
@@ -9,13 +9,22 @@ import java.util.Map;
 public class AbstractCoffee implements Coffee{
 
     void printAllComponentTypes(Map<ComponentType, Integer> unicComponents, CoffeType cofeeType) {
-        System.out.println(changeCase(cofeeType.toString()) + ": {");
+
+ /*       Object[] possibleValues = ComponentType.class.getEnumConstants();
+        System.out.println("---------------------\n" + possibleValues[0]);
+        System.out.println( possibleValues[1]);
+       // enumValue.getE
+*/
+        for (Map.Entry<ComponentType, Integer> entry : unicComponents.entrySet()) {
+            System.out.println("Компонент =  " + entry.getKey() + " Количество = " + entry.getValue());
+        }
+  /*      System.out.println(changeCase(cofeeType.toString()) + ": {");
         System.out.println("Coffee " + unicComponents.get(ComponentType.COFFEE));
         System.out.println("Water " + unicComponents.get(ComponentType.WATER));
         System.out.println("Sugar " + unicComponents.get(ComponentType.SUGAR));
         System.out.println("Milk " + unicComponents.get(ComponentType.MILK));
         System.out.println("}");
-    }
+*/    }
 
     Integer getComponentCountAllTypes(ComponentType componentType, CoffeType coffeType, List<ComponentType> components) {
         System.out.println(changeCase(componentType.toString())  + " в " + changeCase(coffeType.toString()) + " = " + createReceiptMap(components).get(componentType));
